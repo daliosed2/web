2bcf6e553f8ecdb49d2cb9691220b7af459c2ae7
daliosed2 <daliosed@icloud.com>
2025-06-14 11:02:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -36,10 +36,3 @@ if (toggleButton) {
     applyMode(useDark);
   });
 }
-
-// 🔁 Agregar script de MailerLite al final
-(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-.push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-ml('account', '1493483');
